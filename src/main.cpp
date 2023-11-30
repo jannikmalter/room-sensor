d5fcf901c622125bb59abe3090bf83d894eecb8d
jannikmalter <jannikmalter@live.de>
2023-11-30 01:04:34 +0100

    self made

--- src/main.cpp
+++ src/main.cpp
@@ -16,35 +16,44 @@
 //Read humidity register
 #define GY21_READ_HUM    0xF5
 
-//Time before give up readind new reg values
-#define GY21_TIMEOUT     100
-
 TwoWire THsens = TwoWire(0);
 //TwoWire I2Ctwo = TwoWire(1);
 
 float temp;
+float humid;
 
-float get_temp() {
+void meas_TH() {
+  float calc;
+
   THsens.beginTransmission(GY21_I2C_ADDR);
   THsens.write(GY21_READ_TEMP);
   THsens.endTransmission(true);
   delay(50);
   THsens.requestFrom(GY21_I2C_ADDR, 3);
-  return (((THsens.read() << 8) | (THsens.read() & 0b11111100)) * 0.002681274f - 46.85f);
+  temp = ((THsens.read() << 8) | (THsens.read() & 0b11111100)) * 0.002681274f - 46.85f;
+
+
+  THsens.beginTransmission(GY21_I2C_ADDR);
+  THsens.write(GY21_READ_HUM);
+  THsens.endTransmission(true);
+  delay(16);
+  THsens.requestFrom(GY21_I2C_ADDR, 3);  
+  uint16_t Srh = (THsens.read() << 8) | (THsens.read() & 0xFC);
+  calc = -6.0 + 125.0 * Srh / 65536.0;
+  humid = calc + (25.0 - temp) * -0.15;
 }
 
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(F("THsens test"));
-
+  Serial.println("Hello!");
   THsens.begin(SDA_1, SCL_1, 100000); 
   //I2Ctwo.begin(SDA_2, SCL_2, 100000);
-
-
-  Serial.println();
 }
 
 void loop() { 
-  Serial.println(get_temp());
+  meas_TH();
+  Serial.print(temp,6);
+  Serial.print("\t");
+  Serial.println(humid,6);
 }
